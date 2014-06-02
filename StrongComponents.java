532fe7cf33a9ca5458371977f5c0e7032f0ce7e5
Li-Kang Weng <eqttonytony@gmail.com>
2014-06-02 23:15:12 +0800

    Create StrongComponents.java
    Ref. of strongly connected component
    http://en.wikipedia.org/wiki/Strongly_connected_component
    http://www.csie.ntnu.edu.tw/~u91029/Component.html

---
+++ StrongComponents.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tony
+ */
+public class StrongComponents {
+  
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+        KosarajuSharirSCC scc = new KosarajuSharirSCC(G);
+
+        // number of connected components
+        int M = scc.count();
+        StdOut.println(M);
+    }
+}
